93b9c933a5b916f980521fa7e027bf2d9ad4a867
unknown <media08@sogo.local>
2023-05-30 11:29:31 +0900

    minor change

--- simpsons_paradox.R
+++ simpsons_paradox.R
@@ -5,20 +5,21 @@ library(tidyverse)
 library(ggthemes)
 ######################
 set.seed(2)
-data <- simulate_simpson(n = 100, groups = 4, r = 0.2, group_prefix = "school_") |> 
+data_temp <- simulate_simpson(n = 100, groups = 4, r = 0.5, group_prefix = "school_") |> 
   mutate(V2 = (V2 + 9) * 10, V1 = .5 * V1 + 1) |> 
-  rename("study_time" = V1, "grade" = V2, "school" = Group)
+  rename("study_time" = V1, "grade" = V2, "school" = Group) |> 
+  mutate(study_time = study_time*.7)
 #####################
-ggplot(data, aes(x = study_time, y = grade)) +
+ggplot(data_temp, aes(x = study_time, y = grade)) +
   geom_point(aes(color = school)) +
   geom_smooth(aes(color = school), method = "lm", se = FALSE) +
   geom_smooth(method = "lm", se = FALSE) +
-  labs(x = "学習時間", y = "正答率")
+  labs(x = "学習時間(h)", y = "正答率")
 #####################
 piyopiyo <- function(){
-  ggplot(data, aes(x = study_time, y = grade)) +
+  ggplot(data_temp, aes(x = study_time, y = grade)) +
     geom_point(alpha = .7) +
-    labs(x = "学習時間", y = "正答率")
+    labs(x = "学習時間(h)", y = "正答率")
 }
 #####################
 piyopiyo() +
